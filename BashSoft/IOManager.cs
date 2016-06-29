6e65545ae53fde608616022dba47e44d68ff923a
llalov <luchoazz@abv.bg>
2016-06-29 23:25:58 +0300

    initial commit

---
+++ BashSoft/IOManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while(subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+
+                //OutputWriter.WriteMessageOnNewLine(currentPath);
+
+                foreach(string directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+                OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+            }
+        }
+    }
+}
---
+++ BashSoft/Launcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            IOManager.TraverseDirectory(@"D:\Pictures");
+        }
+    }
+}
